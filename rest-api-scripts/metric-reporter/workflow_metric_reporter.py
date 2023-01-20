afc8d7ee7008d42cae458844a2fe641d67e34837
SparkflowsGaurav <106712495+SparkflowsGaurav@users.noreply.github.com>
2023-01-20 14:04:49 -0800

    Update workflow_metric_reporter.py
    Fixed documentation

--- rest-api-scripts/metric-reporter/workflow_metric_reporter.py
+++ rest-api-scripts/metric-reporter/workflow_metric_reporter.py
@@ -8,7 +8,7 @@ from datetime import datetime
 ************Workflow Metric Reporter************
 ----------------------
 Script: workflow_metric_reporter.py
-This script will export a csv file containing the latency and status of all execution of a specified project_id. The summary argument is a Boolean, if set to True additional metrics will be exported (longest_latency, status_count, oldest executions).
+This script will export a csv file containing the latency and status of all execution of a specified project_id. The summary argument is an optional argument if included will generate descriptive csv files (longest_latency, status_count, oldest executions).
 command:  
    python workflow_metric_reporter.py --fire_host_url="https://localhost:8080" --access_token="cacaksncaskjuuonn777-cdck" --project_id="1" --summary  
 
