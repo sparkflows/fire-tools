63808c7b2d03e6b234dd40dde716e821545359c7
ShreenSharma2411 <51231386+ShreenSharma2411@users.noreply.github.com>
2023-05-08 16:41:35 +0530

    Update execute_pipeline.py

--- rest-api-scripts/execute_pipeline.py
+++ rest-api-scripts/execute_pipeline.py
@@ -5,22 +5,22 @@ import json
 '''
 ************Execute pipeline************
 ----------------------
-Script: execute_pipline.py
-
-This script executes inputted workflows and outputs a link to the execution result. It takes in a firehosturl, an access token, and a pipe-separated pipeline id.
-
+Script: execute_pipeline.py
+This script executes inputted workflows and outputs a link to the execution result. It takes in a firehosturl, an access token,pipeline name and project id.
 command:  
-   python execute_pipline.py --fire_host_url="https://localhost:8080" --access_token="cacaksncaskjuuonn777-cdck" --pipeline_ids="1234"
-   
+   python execute_pipeline.py --fire_host_url="https://localhost:8080" --access_token="cacaksncaskjuuonn777-cdck" --pipeline_name="test_pipeline" --project_id=1"
+
    This command executes pipeline 1234 and outputs the link to the execution result
-  
- 
+
+
 ----------------------
 *************************************************
 '''
-def execute_pipeline(fire_host,token,pipeline_id):
-    print(pipeline_id)
-    execute_pipeline_api_url = fire_host + "/api/v1/create/pipelineExecution/" + pipeline_id
+
+
+def execute_pipeline(fire_host, token, pipeline_name,project_id):
+    print(pipeline_name)
+    execute_pipeline_api_url = (fire_host + "/executePipeline?pipelineName={name}&projectId={id}").format(name=str(pipeline_name),id=str(project_id))
     api_call_headers = {
         "token": token
     }
@@ -28,44 +28,27 @@ def execute_pipeline(fire_host,token,pipeline_id):
         "userName": "admin",
         "workflowParameters": ""
     }
-    execute_pipeline_api_url_response = requests.post(execute_pipeline_api_url, json=data, headers=api_call_headers)
-    print(execute_pipeline_api_url_response.status_code)
-    print(execute_pipeline_api_url)
-    get_execution_results(fire_host, token, str(38))
-    if(execute_pipeline_api_url_response.status_code == 200):
-        print("LINK TO EXEUCTION RESULT IS:   " + fire_host + "/#/view-workflow-result/" + execute_pipeline_api_url_response.text)
-        print("THE RESULT OF THE EXECUTION IS SHOWN BELOW")
-        get_execution_results(fire_host, token, execute_pipeline_api_url_response.text)
-        #print(execute_pipeline_api_url_response.json())
-
-def get_execution_results(fire_host, token, execution_id):
-    execution_results_url = fire_host + "/api/v1/pipelines/execution/" + execution_id
-    api_call_headers = {'token': token}
-    execution_results_response = requests.get(execution_results_url, headers=api_call_headers)
-    status_code = execution_results_response.status_code
-    if(status_code == 200):
-        execution_results_text_response = execution_results_response.text
-        #print(execution_results_text_response)
-        execution_results_dict = json.loads(execution_results_text_response)
-        print(execution_results_dict)
-    #print(execution_results_url)
-
+    execute_pipeline_api_response = requests.post(execute_pipeline_api_url, json=data, headers=api_call_headers)
+    print(execute_pipeline_api_response.status_code)
+    if execute_pipeline_api_response.status_code == 200:
+        print(execute_pipeline_api_response.text)
 
 
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser(allow_abbrev=False)
     args_parser.add_argument('--fire_host_url', help='Host URL is required', type=str, required=True)
     args_parser.add_argument('--access_token', help='Access Token is required', type=str, required=True)
-    args_parser.add_argument('--pipeline_ids', help='Pipe Separated pipeline ids', type=str, required=True)
+    args_parser.add_argument('--pipeline_name', help='Pipeline name is required', type=str, required=True)
+    args_parser.add_argument('--project_id', help='Pipeline id is required', type=int, required=True)
     args = args_parser.parse_args()
 
     fire_host_url = args.fire_host_url
     access_token = args.access_token
-    pipeline_ids = args.pipeline_ids
+    pipeline_name = args.pipeline_name
+    project_id = args.project_id
     print("fire_host_url: " + fire_host_url)
     print("access_token: " + access_token)
-    print("pipeline_ids: " + pipeline_ids)
+    print("pipeline_name: " + pipeline_name)
+    print("project_id: "+str(project_id))
 
-    for pipeline in pipeline_ids.split('|'):
-        execute_pipeline(fire_host_url, access_token, pipeline)
-
+    execute_pipeline(fire_host_url, access_token, pipeline_name,project_id)
